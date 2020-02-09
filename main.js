b5b0d6c1fd596e42749d931ec8c236a2fbed4a4f
Susan Khatri <khatrisusan>
2020-02-09 09:35:50 +0100

    added html

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    initialize();
+    init();
 });
+function init(){
+    fetchData();
+}
+function fetchData() {
 
-function initialize(){
-    console.log("hey");
+    fetch("student.json")
+        .then(res => res.json())
+        .then(student)
 }
 
+function student(data) {
+    const myArray = data;
+    myArray.forEach(getStudent);
+
+    //console.log(myArray);
+}
+function getStudent(data) {
+    console.log(data);
+    const clone = document.querySelector("template").content;
+    const cloneData = clone.cloneNode("true");
+
+    cloneData.querySelector("#name").textContent =
+            data.fullname;
+            cloneData.querySelector("#house").textContent =
+            data.house;
+
+            document.querySelector("main").appendChild(cloneData);
+}
